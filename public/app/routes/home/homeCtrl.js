24fc949eedf6120b006ccf058fc45c444bddecc6
olsondeven <olsondeven@gmail.com>
2017-03-09 00:03:20 -0700

    update

--- public/app/routes/home/homeCtrl.js
+++ public/app/routes/home/homeCtrl.js
@@ -4,41 +4,51 @@ angular.module('app').controller('optionOneCtrl', function($scope, $parse, $stat
   //   this.name = name;
   //   this.content = content;
   // }
-  //Declare "variables" $scope properties/keys, this will allow me to display them
-        $scope.folderFiles = [];
-        $scope.strArray = [];
-        $scope.foundFile = [];
+  //Declaring variables or $scope properties/keys
+        let folderFiles = new Array;
+        let strArray = new Array;
+        $scope.foundFile = new Array;
+        $scope.found = false;
         // vanilla javascript to grab the folder with 100 files in it
         // create input box that takes in folder
         // eventlistener for when the files have been uploaded
         document.getElementById("openFile").addEventListener('change', function() {
           // Set values to correct values
-            $scope.folderFiles = this.files;
+            folderFiles = this.files;
           // for loop and store content on file
-            for(let i = 0; i < $scope.folderFiles.length; i++) {
+            for(let i = 0; i < folderFiles.length; i++) {
                 var fr = new FileReader();
 
                 fr.onload = function(){
-                  $scope.strArray.push({content:this.result,name: $scope.folderFiles[i].name});
+                  strArray.push({content:this.result,name: folderFiles[i].name});
                 }
                 //this allows for filereader to extract content of file as a string,
                 //the result of readAsText is a string
-                //passed var i as an argument so I would have the value of i in the onload function
-                fr.readAsText($scope.folderFiles[i],i);
+                //passed var i as an argument so I would have the value of i in the onload fn
+                fr.readAsText(folderFiles[i],i);
             }
-            console.log($scope.folderFiles);
-            console.log($scope.strArray);
-            //questions to ask Bruno
-            //is the phrase case sensitive
-            //return the file name ? or display the file that has the phrase
         });//end of upload
 
         //set phrase from user function, takes in a string and matches any files that
         //have that string
-        $scope.setPhrase = function(phrase){
-
+        $scope.setPhrase = (phrase)=>{
+          //questions to ask Bruno
+          //is the phrase case sensitive
+          //return the file name ? or display the file that has the phrase
+          let regPhrase = phrase;
+          if(regPhrase === ""){
+            regPhrase = 'Find me';
+          }
+          strArray.forEach(function(element,index){
+            if(element.content.match() > 0){
+              $scope.foundFile.push(element);
+              $scope.found = true;
+            }
+          });
           console.log('SetPhrase fn fired',phrase);
+          console.log(folderFiles);
+          console.log(strArray);
 
-        }
+        };
 
     }) //closing
