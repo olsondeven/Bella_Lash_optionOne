3a0b8563521aaef19b55fac397c12e388e1bd984
olsondeven <olsondeven@gmail.com>
2017-03-08 18:17:18 -0700

    setting up input for file

--- public/app/routes/home/homeCtrl.js
+++ public/app/routes/home/homeCtrl.js
@@ -1,3 +1,21 @@
 angular.module('app').controller('homeCtrl',function($scope, $stateParams, mainService, $rootScope){
-  $scope.test = "HELLO WORLD";
+
+  $scope.takeFile = function(file){
+    console.log(file);
+  };
+
+
+
+
+  //vanilla javascript
+  document.getElementById("openFile").addEventListener('change',function(){
+    var fr = new FileReader();
+    console.log(fr);
+    fr.onload = function(){
+      document.getElementById("fileContents").textContent = this.result;
+    }
+    fr.readAsText(this.files[0]);
+  });
+
+
 })//closing
