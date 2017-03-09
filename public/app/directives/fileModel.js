53531df62a52ab5442bbe8cea091d160e02556a9
olsondeven <olsondeven@gmail.com>
2017-03-08 19:20:41 -0700

    update to directive

--- public/app/directives/fileModel.js
+++ public/app/directives/fileModel.js
@@ -2,18 +2,18 @@ angular.module('app').directive('fileModel', function($parse) {
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
-            
+            //declare needed variables
             var model = $parse(attrs.fileModel);
-            console.log(model);
-            console.log(model.assign);
             var modelSetter = model.assign;
-            console.log(modelSetter);
+            //this event listener allows file to be uploaded when button is clicked.
             element.bind('change', function() {
                 scope.$apply(function() {
                     modelSetter(scope, element[0].files[0]);
-                    console.log(modelSetter);
                 });
             });
+           element.bind('change',function(){
+
+           });
         }
     }; //return closing
 }); //closing
