f0fdd0f8d378ad22cab85540a75e13d4f5ae51a0
olsondeven <olsondeven@gmail.com>
2017-03-08 21:36:12 -0700

    fix to folder being uploaded instead of one file

--- public/app/directives/fileModel.js
+++ public/app/directives/fileModel.js
@@ -2,18 +2,7 @@ angular.module('app').directive('fileModel', function($parse) {
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
-            //declare needed variables
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
-            //this event listener allows file to be uploaded when button is clicked.
-            element.bind('change', function() {
-                scope.$apply(function() {
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-           element.bind('change',function(){
 
-           });
-        }
+            } //link closing
     }; //return closing
 }); //closing
--- public/app/routes/home/homeCtrl.js
+++ public/app/routes/home/homeCtrl.js
@@ -1,23 +1,26 @@
-angular.module('app').controller('homeCtrl',function($scope, $parse, $stateParams, mainService, $rootScope){
+angular.module('app').controller('homeCtrl', function($scope, $parse, $stateParams, mainService, $rootScope) {
+  //Declare "variables" $scope properties/keys, this will allow me to display them
+        $scope.folderFiles = new Array;
+        $scope.strArray = new Array;
+        $scope.foundFile = null;
+        var phrase = "";
+        // vanilla javascript to grab the folder with 100 files in it
+        // create input box that takes in folder
+        // eventlistener for when the files have been uploaded
+        document.getElementById("openFile").addEventListener('change', function() {
+          // Set values to correct values
+            $scope.folderFiles = this.files;
+          // for loop and store content on file
+            for (var i = 0; i < $scope.folderFiles.length; i++) {
+                var fr = new FileReader();
+                fr.onload = function(){
+                  $scope.strArray.push({content:this.result});
+                  // $scope.folderFiles[i].content = this.result;
+                }
+                fr.readAsText($scope.folderFiles[i]);
+            }
+            console.log($scope.folderFiles6);
+            console.log($scope.strArray);
+        });
 
-  $scope.uploadFile = function() {
-      var file = $scope.myFile;
-
-      console.log('file is ');
-      console.dir(file);
-
-      var fr = new FileReader();
-
-  };
-
-  //vanilla javascript
-  // document.getElementById("openFile").addEventListener('change',function(){
-  //   var fr = new FileReader();
-  //   console.log(fr);
-  //   fr.onload = function(){
-  //     document.getElementById("fileContents").textContent = this.result;
-  //   }
-  //   fr.readAsText(this.files[0]);
-  // });
-
-})//closing
+    }) //closing
